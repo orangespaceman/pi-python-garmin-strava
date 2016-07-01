c4883ce8527f7285041133991a286363f88c6159
Pete Goodman <thegingerbloke@users.noreply.github.com>
2016-07-01 22:03:15 +0100

    Allow extra time for the device to mount

--- bash/on-garmin-add.sh
+++ bash/on-garmin-add.sh
@@ -3,6 +3,22 @@
 # allow the garmin time to mount
 sleep 10
 
+COUNTER=0;
+while [  $COUNTER -lt 5 ]; do
+
+    echo Attempt $COUNTER
+    let COUNTER=COUNTER+1
+
+    if df -h | grep -q "/media/usb0"
+    then
+        echo "DEVICE FOUND"
+        break
+    else
+        echo "DEVICE NOT FOUND"
+        sleep 10
+    fi
+done
+
 # grab the path we've passed through
 DIRPATH=$1;
 
